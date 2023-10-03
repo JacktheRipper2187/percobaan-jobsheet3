8ea79dfdaf7f9e7b6751fbfa274c8dfd72d6ca61
JacktheRipper2187 <145539398+JacktheRipper2187@users.noreply.github.com>
2023-10-04 02:07:11 +0700

    Add files via upload
    upload file java

---
+++ ContohVariabel22.java
@@ -0,0 +1,16 @@
+public class ContohVariabel22 {
+    public static void main(String[] args) {
+
+String salahSatuHobbySayaAdalah = "Bermain Basket";
+boolean isPandai = true;
+char jenisKelamin = 'L';
+byte _umurSayaSekarang = 18;
+double $ipk = 3.99, tinggi = 1.68;
+System.out.println(salahSatuHobbySayaAdalah);
+System.out.println("Apakah pandai?" + isPandai);
+System.out.println("Jenis kelamin:" + jenisKelamin);
+System.out.println("Umurku saat ini:" + _umurSayaSekarang);
+System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+}
