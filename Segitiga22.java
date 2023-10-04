abdbb8ada6ce59c3d5db84a6d88c3206ab922e6d
JacktheRipper2187 <ulilf442@gmail.com>
2023-10-04 19:56:51 +0700

    mengganti golongan darah

--- Segitiga22.java
+++ Segitiga22.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Segitiga22 {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         int alas, tinggi;
